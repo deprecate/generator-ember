04734a41bfcb373321d49c9237c75568009d57cd
Loïc Minaudier <loic.minaudier@gmail.com>
2013-04-25 17:56:12 +0300

    Add support for deeply nested handlebars templates

--- app/templates/Gruntfile.js
+++ app/templates/Gruntfile.js
@@ -301,7 +301,7 @@ module.exports = function (grunt) {
             },
             dist: {
                 files: {
-                    '.tmp/scripts/compiled-templates.js': '<%%= yeoman.app %>/templates/{,*/}*.hbs'
+                    '.tmp/scripts/compiled-templates.js': '<%%= yeoman.app %>/templates/{,**/}*.hbs'
                 }
             }
         }
