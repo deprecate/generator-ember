cbf59d3544bdc2816420ad5d226ec8b0bb0e8112
Anthony Bull <anthony@bluxomelabs.com>
2013-07-13 14:26:09 -0700

    Passing attrs for model creation (no tests)

--- model/index.js
+++ model/index.js
@@ -4,6 +4,18 @@ var yeoman = require('yeoman-generator');
 
 var ModelGenerator = module.exports = function ModelGenerator(args, options, config) {
   yeoman.generators.NamedBase.apply(this, arguments);
+
+  // XXX default and banner to be implemented
+  this.argument('attributes', { type: Array, defaults: [], banner: 'field[:type] field[:type]' });
+
+  // parse back the attributes provided, build an array of attr
+  this.attrs = this.attributes.map(function(attr) {
+    var parts = attr.split(':');
+    return {
+      name: parts[0],
+      type: parts[1] || 'string'
+    };
+  });
 };
 
 util.inherits(ModelGenerator, yeoman.generators.NamedBase);
--- model/templates/base.js
+++ model/templates/base.js
@@ -1,4 +1,3 @@
-<%= _.classify(appname) %>.<%= _.classify(name) %> = DS.Model.extend({
-  // Implement your model here.
-});
-
+<%= _.classify(appname) %>.<%= _.classify(name) %> = DS.Model.extend({<% _.each(attrs, function(attr, i) { %>
+  <%= _.camelize(attr.name) %>: DS.attr('<%= attr.type %>')<% if(i < (attributes.length - 1)) { %>,<% } %>
+<% }); %>});
