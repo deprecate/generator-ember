36db918c0d3902f7151a7eb0800f8495433591af
Anthony Bull <anthony@bluxomelabs.com>
2013-07-13 14:40:14 -0700

    Basic view creation (no hbs, no tests)

---
+++ view/index.js
@@ -0,0 +1,17 @@
+'use strict';
+var util = require('util');
+var yeoman = require('yeoman-generator');
+
+var ViewGenerator = module.exports = function ViewGenerator(args, options, config) {
+  // By calling `NamedBase` here, we get the argument to the subgenerator call
+  // as `this.name`.
+  yeoman.generators.NamedBase.apply(this, arguments);
+
+  console.log('You called the view subgenerator with the argument ' + this.name + '.');
+};
+
+util.inherits(ViewGenerator, yeoman.generators.NamedBase);
+
+ViewGenerator.prototype.files = function files() {
+  this.copy('base.js', 'app/scripts/views/' + this._.slugify(this.name) + '_view.js');
+};
