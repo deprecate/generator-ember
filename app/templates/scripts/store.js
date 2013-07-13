22a4acb0d54b58d6f9176e09f8a502bea08dfd1e
Anthony Bull <anthony@bluxomelabs.com>
2013-07-13 15:42:34 -0700

    Living conservatively; don't assume Ember Data for now

--- app/templates/scripts/store.js
+++ app/templates/scripts/store.js
@@ -1,3 +1,6 @@
 <% if (emberData) { %>
-<%= _.classify(appname) %>.Store = DS.Store.extend({
-});<% } %>
+// if you're looking at this, you probably know what you're doing...
+if(DS){
+  <%= _.classify(appname) %>.Store = DS.Store.extend({});
+}
+<% } %>
