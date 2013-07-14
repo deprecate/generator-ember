a7cc44b0c87c00bfd6cebe7f4f5710f76ce200b8
Anthony Bull <anthony@bluxomelabs.com>
2013-07-13 18:52:32 -0700

    Refactor of router subgen (in bid to get tests to pass)

--- router/index.js
+++ router/index.js
@@ -4,6 +4,15 @@ var yeoman = require('yeoman-generator');
 var fs = require("fs")
 
 var RouterGenerator = module.exports = function RouterGenerator(args, options, config) {
+  // NamedBase needs a name, which is usually the first param passed in the script
+  // https://github.com/yeoman/generator/pull/231
+  args.push('ember:router'); 
+  yeoman.generators.NamedBase.apply(this, arguments);
+};
+
+util.inherits(RouterGenerator, yeoman.generators.NamedBase);
+
+RouterGenerator.prototype.files = function files() {
   this.attrs = [];
   var files = fs.readdirSync('app/scripts/controllers');
   for(var i in files) {
