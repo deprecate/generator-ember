1186ce95f6bcf8dde0e6101b8e72c26dc08b8893
Anthony Bull <anthony@bluxomelabs.com>
2013-07-13 18:05:01 -0700

    Picking out routes from controller dir for dynamic gen

--- router/index.js
+++ router/index.js
@@ -1,7 +1,13 @@
 'use strict';
 var util = require('util');
 var yeoman = require('yeoman-generator');
+var fs = require("fs")
 
 var RouterGenerator = module.exports = function RouterGenerator(args, options, config) {
+  this.attrs = [];
+  var files = fs.readdirSync('app/scripts/controllers');
+  for(var i in files) {
+    this.attrs.push( files[i].replace('_controller.js','') );
+  }
   this.copy('base.js', 'app/scripts/router.js');
 };
--- router/templates/base.js
+++ router/templates/base.js
@@ -1,3 +1,7 @@
 <%= _.classify(appname) %>.Router.map(function () {
+  <% _.each(attrs, function(attr, i) { %>
+    this.route('<%= attr %>');
+  <% }); %>
 });
+
 
