e9c35ca3c1239217be39ecc18a5e729ddf2e90c6
Anthony Bull <anthony.bull@crowdflower.com>
2013-07-15 10:29:34 -0700

    Added task to invoke PhantomJS connection during tests
    Fixes:
    
    https://github.com/yeoman/generator-ember/issues/77
    
    When running `grunt test`, was seeing:
    
    Warning: PhantomJS unable to load "http://localhost:9000/index.html" URI. Use --force to continue.
    
    Thanks @benperiton and @fayimora

--- app/templates/Gruntfile.js
+++ app/templates/Gruntfile.js
@@ -365,6 +365,7 @@ module.exports = function (grunt) {
     grunt.registerTask('test', [
         'clean:server',
         'concurrent:test',
+        'connect:test',
         'neuter:app',<% if (options.karma) { %>
         'karma',<% } else if (testFramework === 'mocha') { %>
         'mocha'<% } else if (testFramework === 'jasmine') { %>
