4b77d37dd5110545f73e876645a690108b05a26d
Anthony Bull <anthony.bull@crowdflower.com>
2013-07-25 08:18:36 -0700

    Add interpolated app name for files gen'd for Karma

--- app/templates/test/initializer.js
+++ app/templates/test/initializer.js
@@ -7,12 +7,12 @@ var should = chai.should();
 chai.Assertion.includeStack = true;
 
 Ember.testing = true;
-App.rootElement = "#ember-testing";
+<%= _.classify(appname) %>.rootElement = "#ember-testing";
 Ember.Test.adapter = Ember.Test.MochaAdapter.create();
 
-App.setupForTesting();
-App.injectTestHelpers();
-Ember.run(App, App.advanceReadiness);
+<%= _.classify(appname) %>.setupForTesting();
+<%= _.classify(appname) %>.injectTestHelpers();
+Ember.run(<%= _.classify(appname) %>, <%= _.classify(appname) %>.advanceReadiness);
 
 window.start = function () {};
 window.stop = function () {};
--- app/templates/test/integration/index.js
+++ app/templates/test/integration/index.js
@@ -8,7 +8,7 @@ describe("Index page", function () {
 
 describe("IndexRoute", function () {
   describe("model property", function () {
-    var indexRoute = App.IndexRoute.create();
+    var indexRoute = <%= _.classify(appname) %>.IndexRoute.create();
     it("should have the right number of items", function () {
       var model = indexRoute.model();
       model.should.have.length(3);
