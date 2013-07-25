182a868837fffe145d2e1a3f9f539e22a36aca62
Anthony Bull <anthony.bull@crowdflower.com>
2013-07-25 07:57:12 -0700

    Factoring Karma tests into own file

--- test/test-karma.js
+++ test/test-karma.js
@@ -22,7 +22,7 @@ var EXPECTED_FILES = [
   'app/index.html'
 ];
 
-describe('Basics', function () {
+describe('Karma', function () {
   beforeEach(function (done) {
 
     helpers.testDirectory(path.join(__dirname, './temp'), function (err) {
@@ -46,40 +46,12 @@ describe('Basics', function () {
     }.bind(this));
   });
 
-  it('every generator can be required without throwing', function () {
-    // not testing the actual run of generators yet
-    this.app = require('../app');
-    this.router = require('../router');
-    this.controller = require('../controller');
-    this.view = require('../view');
-    this.model = require('../model');
-  });
-
-  it('creates the expected files', function (done) {
+  it('creates config file when using karma-runner', function (done) {
+    this.ember.app.options['karma'] = true;
     this.ember.app.run({}, function () {
-      helpers.assertFiles(EXPECTED_FILES);
+      assert.ok(fs.existsSync('karma.conf.js'));
       done();
     });
   });
 
-  it('properly links ember data', function (done) {
-    var expected = [
-      [
-        'app/index.html', 
-        /<script src="bower_components\/ember-data-shim\/ember-data.js"><\/script>/
-      ]
-    ];
-    this.ember.app.run({}, function () {
-      helpers.assertFiles(expected);
-      done();
-    });
-  });
-
-  it('creates a CoffeeScript based project when --coffee is specified', function (done) {
-    this.ember.app.options['coffee'] = true;
-    this.ember.app.run({}, function () {
-      assert.ok(fs.existsSync('app/scripts/app.coffee'));
-      done();
-    });
-  });
 });
