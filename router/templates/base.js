6f2639ca153094438ab781fdcd2c6760f5aa8973
Anthony Bull <anthony@bluxomelabs.com>
2013-08-04 18:04:57 -0700

    whitespace

--- router/templates/base.js
+++ router/templates/base.js
@@ -1,6 +1,6 @@
 <%= _.classify(appname) %>.Router.map(function () {
   // Register routes
-    <% _.each(attrs, function(attr, i) { %>
-        this.route('<%= attr %>');
-    <% }); %>
+  <% _.each(attrs, function(attr, i) { %>
+  this.route('<%= attr %>');
+  <% }); %>
 });
