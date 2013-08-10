4cdaca48cead0d4a5e0bc5f89e340695a5100afd
Anthony Bull <anthony@bluxomelabs.com>
2013-08-10 13:18:32 -0700

    Add production of auto-populated fixures

--- model/index.js
+++ model/index.js
@@ -26,4 +26,5 @@ util.inherits(ModelGenerator, yeoman.generators.NamedBase);
 
 ModelGenerator.prototype.files = function files() {
   this.copy('base.js', 'app/scripts/models/' + this._.slugify(this.name) + '_model.js');
+  this.copy('fixtures.js', 'app/scripts/models/' + this._.slugify(this.name) + '_fixtures.js');
 };
