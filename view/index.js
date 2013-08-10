cef8009da845fa885e196e10fe3a68c265cb7f44
Anthony Bull <anthony@bluxomelabs.com>
2013-08-10 15:10:15 -0700

    wip: getting linkTo to work for singles

--- view/index.js
+++ view/index.js
@@ -6,13 +6,14 @@ var fleck = require('fleck');
 var ViewGenerator = module.exports = function ViewGenerator(args, options, config) {
   yeoman.generators.NamedBase.apply(this, arguments);
   this.pluralized_name = fleck.pluralize(this.name);
+  this.slugified_name = this._.slugify(this.name);
 };
 
 util.inherits(ViewGenerator, yeoman.generators.NamedBase);
 
 ViewGenerator.prototype.files = function files() {
-  this.copy('single.js', 'app/scripts/views/' + this._.slugify(this.name) + '_view.js');
+  this.copy('single.js', 'app/scripts/views/' + this.slugified_name + '_view.js');
   this.copy('plural.js', 'app/scripts/views/' + this._.slugify(this.pluralized_name) + '_view.js');
-  this.copy('single.hbs', 'app/templates/' + this._.slugify(this.name) + '.hbs');
+  this.copy('single.hbs', 'app/templates/' + this.slugified_name + '.hbs');
   this.copy('plural.hbs', 'app/templates/' + this._.slugify(this.pluralized_name) + '.hbs');
 };
