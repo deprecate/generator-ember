7dae5e6b75dfd3aa9c4a60caf8bdabffef211761
Anthony Bull <anthony@bluxomelabs.com>
2013-08-10 14:16:23 -0700

    Fixed pluralization in users view

--- view/index.js
+++ view/index.js
@@ -5,14 +5,14 @@ var fleck = require('fleck');
 
 var ViewGenerator = module.exports = function ViewGenerator(args, options, config) {
   yeoman.generators.NamedBase.apply(this, arguments);
+  this.pluralized_name = fleck.pluralize(this.name);
 };
 
 util.inherits(ViewGenerator, yeoman.generators.NamedBase);
 
 ViewGenerator.prototype.files = function files() {
-  var pluralized_name = fleck.pluralize(this.name);
   this.copy('single.js', 'app/scripts/views/' + this._.slugify(this.name) + '_view.js');
-  this.copy('plural.js', 'app/scripts/views/' + this._.slugify(pluralized_name) + '_view.js');
+  this.copy('plural.js', 'app/scripts/views/' + this._.slugify(this.pluralized_name) + '_view.js');
   this.copy('single.hbs', 'app/templates/' + this._.slugify(this.name) + '.hbs');
-  this.copy('plural.hbs', 'app/templates/' + this._.slugify(pluralized_name) + '.hbs');
+  this.copy('plural.hbs', 'app/templates/' + this._.slugify(this.pluralized_name) + '.hbs');
 };
