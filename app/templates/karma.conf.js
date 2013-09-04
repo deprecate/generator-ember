2a6364b73db19aa6c3925a64b846940ffbb7b93c
José Mota <jose@josemota.net>
2013-09-04 17:50:26 +0100

    Update karma configuration.
    Commit summary:
    
    * Set testFramework option in the `frameworks` variable.
    
      > In order to clear the deprecation warning, is is now necessary to define
      > the framework choice in its place.
    
    * Add module.exports as karma now requires it.
    * Fix log level definition.
    * Properly use the module pattern, that way Karma behaves well.
    * Set up the proper npm dependencies.

--- app/templates/karma.conf.js
+++ app/templates/karma.conf.js
@@ -1,80 +1,95 @@
 /*jshint undef:false */
 
 // Karma configuration
+module.exports = function(config) {
 
-// base path, that will be used to resolve files and exclude
-basePath = '';
+  config.set({
 
+    // Base path that will be used to resolve files and exclude.
+    basePath : '',
 
-// list of files / patterns to load in the browser
-files = [
-  <% if (testFramework === 'mocha') { %>MOCHA,
-  MOCHA_ADAPTER,<% } else { %>
-  JASMINE,
-  JASMINE_ADAPTER,<% } %>
-  'app/bower_components/jquery/jquery.min.js',
-  'app/bower_components/handlebars/handlebars.runtime.js',
-  'app/bower_components/ember/ember.js',
-  'app/bower_components/ember-data-shim/ember-data.js',
-  <% if (testFramework === 'mocha') { %>
-  'test/lib/chai.js',
-  'app/bower_components/ember-mocha-adapter/adapter.js',
-  <% } %>
-  '.tmp/scripts/combined-scripts.js',
-  '.tmp/scripts/compiled-templates.js',<% if (options.coffee) { %>
-  'test/support/*.coffee',
-  'test/integration/*.coffee'<% } else { %>
-  'test/support/*.js',
-  'test/integration/*.js'<% } %>
-];
+    // Testing framework to be used, default is `jasmine`.
+    frameworks : [
+      <% if (testFramework === 'mocha') { %>'mocha'
+      <% } else                         { %>'jasmine'
+      <% } %>
+    ],
 
 
-// list of files to exclude
-exclude = [];
+    // List of files / patterns to load in the browser.
+    files : [
+      'app/bower_components/jquery/jquery.min.js',
+      'app/bower_components/handlebars/handlebars.runtime.js',
+      'app/bower_components/ember/ember-1.0.0-rc.6.1.js',
+      'app/bower_components/ember-data-shim/ember-data.js',
+      <% if (testFramework === 'mocha') { %>
+      'test/lib/chai.js',
+      'app/bower_components/ember-mocha-adapter/adapter.js',
+      <% } %>
+      '.tmp/scripts/combined-scripts.js',
+      '.tmp/scripts/compiled-templates.js',<% if (options.coffee) { %>
+      'test/support/*.coffee',
+      'test/integration/*.coffee'<% } else { %>
+      'test/support/*.js',
+      'test/integration/*.js'<% } %>
+    ],
 
 
-// test results reporter to use
-// possible values: 'dots', 'progress', 'junit'
-reporters = ['progress'];
+    // List of files to exclude.
+    exclude : [],
 
 
-// web server port
-port = 9876;
+    // Test results reporter to use.
+    // Possible values: 'dots', 'progress', 'junit'
+    reporters : ['progress'],
 
 
-// cli runner port
-runnerPort = 9100;
+    // Web server port.
+    port : 9876,
 
 
-// enable / disable colors in the output (reporters and logs)
-colors = true;
+    // Cli runner port.
+    runnerPort : 9100,
 
 
-// level of logging
-// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-logLevel = LOG_INFO;
+    // Enable / disable colors in the output (reporters and logs).
+    colors : true,
 
 
-// enable / disable watching file and executing tests whenever any file changes
-autoWatch = true;
+    // Level of logging. Possible values are:
+    //
+    // * LOG_DISABLE
+    // * LOG_ERROR
+    // * LOG_WARN
+    // * LOG_INFO
+    // * LOG_DEBUG
+    logLevel : config.LOG_INFO,
 
 
-// Start these browsers, currently available:
-// - Chrome
-// - ChromeCanary
-// - Firefox
-// - Opera
-// - Safari (only Mac)
-// - PhantomJS
-// - IE (only Windows)
-browsers = ['Chrome'];
+    // Enable / disable watching files and executing tests whenever any of them
+    // changes.
+    autoWatch : true,
 
 
-// If browser does not capture in given timeout [ms], kill it
-captureTimeout = 60000;
+    // Start these browsers, currently available:
+    // - Chrome
+    // - ChromeCanary
+    // - Firefox
+    // - Opera
+    // - Safari (only Mac)
+    // - PhantomJS
+    // - IE (only Windows)
+    browsers : ['Chrome'],
 
 
-// Continuous Integration mode
-// if true, it capture browsers, run tests and exit
-singleRun = false;
+    // If the browser does not capture in the given timeout [ms], then kill it.
+    captureTimeout : 60000,
 
+
+    // Continuous Integration mode.
+    // If it's `true`, then it captures browsers, runs the tests and exits.
+    singleRun : false
+
+  });
+
+};
