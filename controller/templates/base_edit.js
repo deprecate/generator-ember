47d64b792f193b88800fbca5ed1d89689ab2e7d3
José Mota <jose@josemota.net>
2013-09-12 10:12:04 +0100

    Remove hardcoded 'user' route transition.

--- controller/templates/base_edit.js
+++ controller/templates/base_edit.js
@@ -2,7 +2,7 @@
   save: function(){
     // we're cheating here that there's no commit()
     // but the UI element is already bound to the model
-    this.transitionToRoute('user',this.get('model'));
+    this.transitionToRoute('<%= name.toLowerCase() %>',this.get('model'));
   }
 });
 
