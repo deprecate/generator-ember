503257c20662b11ac595d9da629c675f5abd61df
Pascal Hartig <passy@twitter.com>
2013-11-25 10:20:24 +0000

    Clean up file creation tests
    Never mock/stub system APIs. Even worse if you're not directly using it, but
    only relying on them down the line.

--- test/test-file-creation.js
+++ test/test-file-creation.js
@@ -1,4 +1,4 @@
-/*global describe:true, beforeEach:true, it:true */
+/*global describe:true, beforeEach:true, it:true, afterEach:true */
 'use strict';
 var path = require('path');
 var helpers = require('yeoman-generator').test;
@@ -22,8 +22,8 @@ var EXPECTED_FILES = [
   'app/index.html'
 ];
 
-describe('Basics', function () {
-  
+describe('File Creation', function () {
+
   beforeEach(function (done) {
 
     helpers.testDirectory(path.join(__dirname, './temp'), function (err) {
@@ -31,34 +31,34 @@ describe('Basics', function () {
         return done(err);
       }
 
-      this.ember = {};
-      this.ember.app = helpers.createGenerator('ember:app', [
+      this.ember = helpers.createGenerator('ember:app', [
         '../../router',
         '../../app', [
           helpers.createDummyGenerator(),
           'mocha:app'
         ]
       ]);
-      helpers.mockPrompt(this.ember.app, {
-        'compassBootstrap': true
+      helpers.mockPrompt(this.ember, {
+        compassBootstrap: true
       });
-      this.ember.app.options['coffee'] = false;
-      this.ember.app.options['skip-install'] = true;
+      this.ember.options.coffee = false;
+      this.ember.options['skip-install'] = true;
+
       done();
     }.bind(this));
   });
-  
+
   it('every generator can be required without throwing', function () {
     // not testing the actual run of generators yet
-    this.app = require('../app');
-    this.router = require('../router');
-    this.controller = require('../controller');
-    this.view = require('../view');
-    this.model = require('../model');
+    require('../app');
+    require('../router');
+    require('../controller');
+    require('../view');
+    require('../model');
   });
 
   it('creates the expected files', function (done) {
-    this.ember.app.run({}, function () {
+    this.ember.run({}, function () {
       helpers.assertFiles(EXPECTED_FILES);
       done();
     });
@@ -67,56 +67,33 @@ describe('Basics', function () {
   it('properly links ember data', function (done) {
     var expected = [
       [
-        'app/index.html', 
+        'app/index.html',
         /<script src="@@ember_data"><\/script>/
       ]
     ];
-    this.ember.app.run({}, function () {
+    this.ember.run({}, function () {
       helpers.assertFiles(expected);
       done();
     });
   });
 
   it('creates a CoffeeScript based project when --coffee is specified', function (done) {
-    this.ember.app.options['coffee'] = true;
-    this.ember.app.run({}, function () {
+    this.ember.options.coffee = true;
+    this.ember.run({}, function () {
       assert.ok(fs.existsSync('app/scripts/app.coffee'));
       assert.ok(!fs.existsSync('app/scripts/router.js'));
       done();
     });
   });
-  
-  describe('the app name', function () {
-    before(function (done) {
-      helpers.stub(path, 'basename', function(dir) { return 'some-app'; });
-      done();
-    })
-    after(function (done) {
-      helpers.restore();
-      done();
-    });
-    
-    it('creates an application object named after the directory usually', function (done) {
-      this.ember.app.run({}, function () {
-        helpers.assertFile('app/scripts/app.js', /var SomeApp = window.SomeApp = Ember\.Application\.create\(\);/);
-        done();
-      });
-    });
-  });
 
-  describe('the app name, special case', function () {
-    before(function (done) {
-      helpers.stub(path, 'basename', function(dir) { return 'ember'; });
-      done();
-    })
-    after(function (done) {
-      helpers.restore();
-      done();
+  describe('the app name', function () {
+    beforeEach(function () {
+      this.ember.appname = 'some-app';
     });
-    
-    it('creates an application object with a special name', function (done) {
-      this.ember.app.run({}, function () {
-        helpers.assertFile('app/scripts/app.js', /var EmberApp = window.EmberApp = Ember\.Application\.create\(\);/);
+
+    it('creates an application object named after the directory usually', function (done) {
+      this.ember.run({}, function () {
+        helpers.assertFile('app/scripts/app.js', /var SomeApp = window.SomeApp = Ember\.Application\.create\(\);/);
         done();
       });
     });
@@ -124,7 +101,7 @@ describe('Basics', function () {
 
   describe('store', function () {
     it('uses FixtureAdapter by default', function (done) {
-      this.ember.app.run({}, function () {
+      this.ember.run({}, function () {
         helpers.assertFile('app/scripts/store.js', /ApplicationAdapter = DS.FixtureAdapter;/);
         done();
       });
