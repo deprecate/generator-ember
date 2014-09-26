782e392d5be714042feeccea7568a16b925baf9f
fourq <r@cky.bz>
2014-09-26 10:01:25 -0500

    Add grunt-bump to help verison application.

--- app/templates/Gruntfile.js
+++ app/templates/Gruntfile.js
@@ -26,6 +26,21 @@ module.exports = function (grunt) {
 
     grunt.initConfig({
         yeoman: yeomanConfig,
+        bump: {
+          options: {
+            files: ["package.json"],
+            updateConfigs: [],
+            commit: true,
+            commitMessage: "Release v%VERSION%",
+            commitFiles: ["-a"],
+            createTag: true,
+            tagName: "v%VERSION%",
+            tagMessage: "Version %VERSION%",
+            push: true,
+            pushTo: "origin",
+            gitDescribeOptions: "--tags --always --abbrev=1 --dirty=-d"
+          }
+        },
         watch: {
             emberTemplates: {
                 files: '<%%= yeoman.app %>/templates/**/*.hbs',
