231187f0d8beb3c0c69eb76f739ef598b785a258
Arthur Verschaeve <contact@arthurverschaeve.be>
2014-11-02 15:23:46 +0100

    Make open:server respect hostname
    Close #241

--- app/templates/Gruntfile.js
+++ app/templates/Gruntfile.js
@@ -100,7 +100,7 @@ module.exports = function (grunt) {
         },
         open: {
             server: {
-                path: 'http://localhost:<%%= connect.options.port %>'
+                path: 'http://<%%= connect.options.hostname %>:<%%= connect.options.port %>'
             }
         },
         clean: {
